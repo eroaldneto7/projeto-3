44581b74215dd2665f311a4c1ffff08a9f777413
Eroaldo Serafim <eroaldoserafim02@gmail.com>
2024-10-10 16:52:08 -0300

    aula10/10

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+        <div id="menu_topo">
+            <img src="imagens/logomarca.png">
+        <div class="btn_dourado"> ÁREA DO CLIENTE</div>
+        
+        </div><!-- FIM do menu topo-->
+
+        <div id="secao1">
+            <div id="formulario1">
+                <img src="imagens/logomarca.png"> <br>
+                <h1>Desvendando os Segredos da Escrita Persuasiva</h1>
+                <p>Aprenda a escrever com impacto e conquiste resultados incríveis. Torne-se um mestre e transforme sua comunicação em vendas!</p>
+                <form action="" method="">
+                    <input type="text" name="nome_digitado" placeholder="insira seu primeiro nome"><br>
+                    <input type="emal" name="email_digitado" placeholder="insira seu melhor email"><br>
+                    <button type="submit">QUERO ME INSCREVER AGORA!</button><br>
+                    <img src="imagens/calendario2.png">
+                    De 02 a 05 de MAIO às 20h | Online e Gratuito
+                </form>
+            </div>
+            <img id="foto1" src="imagens/foto1.png">
+
+        </div><!-- fim do secao1-->
+
+        <div id="secao2">
+            <div class="cartoes">
+                <div class="icones"><img src="imagens/casa.png"></div>
+                <span>AULA 1</span><br>
+                <H4>Os Fundamentos da Copywriting</H4>
+                <P>Descubra como identificar seu público-alvo, compreender suas necessidades e transformar essas percepções em mensagens persuasivas, utilizando gatilhos mentais poderosos</P>
+            </div> <!--FIM DO CARTAO 1-->
+
+             <div class="cartoes">
+                <div class="icones"><img src="imagens/money.png"></div>
+                <span>AULA 2</span><br>
+                <H4>Técnicas Avançadas de Copywriting</H4>
+                <P>Aprenda a criar headlines impactantes, desenvolver o poder da narrativa e dominar a escrita persuasiva para diferentes canais de comunicação, como websites, e-mails, anúncios e redes sociais.</P>
+
+            </div> <!--FIM DO CARTAO 2-->
+
+            <div class="cartoes">
+                <div class="icones"><img src="imagens/fire.png"></div>
+                <span>AULA 3</span><br>
+                <H4>Copywriting em Ação: Estudos de Caso</H4>
+                <P>Desvendando estratégias por trás de campanhas bem-sucedidas, e obtenha insights valiosos para aplicar em seus próprios projetos, aprimorando suas táticas eficazes de copywriting.</P>
+
+            </div><!--fim do cartao3-->
+
+
+        </div><!--fimada secao2-->
+
+        <div id="secao3">
+             <img src="imagens/foto2.png">
+             <div id="conteudo3">
+                <h1>Para quem este evento é obrigatorio</h1>
+
+                <div class="cartoes2">
+                    <div class="verificado"><img src="imagens/verificado.png"></div>
+                    <p>Empreendedores que desejam aumentar suas taxas de conversão e impulsionar suas vendas através de mensagens persuasivas.</p>
+        </div><!--fim da secao3-->
+    
+</body>
+</html>
